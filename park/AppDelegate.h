07b5221af1aa1e48b8f85a78331fecadc0c20675
Jon Wheatley <jonwheatley@gmail.com>
2012-04-23 16:05:19 -0700

    Initial Commit

---
+++ park/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  park
+//
+//  Created by Jon Wheatley on 4/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
